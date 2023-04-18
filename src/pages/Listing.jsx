602594314b0692aa8cf322d8f393420f16a959ae
mnmt <mnmtsnghh@gmail.com>
2023-04-18 21:04:51 +0530

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -12,11 +12,13 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
   const { listingId } = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
   useEffect(() => {
@@ -48,7 +50,7 @@ export default function Listing() {
         navigation
         pagination={{ type: "progressbar" }}
         effect="fade"
-        modules={[EffectFade]}
+        // modules={[EffectFade]}
         autoplay={{ delay: 3000 }}
       >
         {listing.imgUrls.map((url, index) => (
@@ -63,6 +65,23 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className=" fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+        {shareLinkCopied && (
+          <span className="absolute -bottom-10 -right-2 bg-gray-400 font-semibold border-2 border-gray-400 rounded-md text-white text-xs px-2 py-1">
+            Copied
+          </span>
+        )}
+      </div>
     </main>
   );
 }
